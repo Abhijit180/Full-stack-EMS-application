ac88b62ab4d3def2606b4db22cee1f11953e12fe
Abhijit180 <abhi.suryawanshi1809@gmail.com>
2024-01-04 12:28:18 +0530

    initial commit

---
+++ ems-frontend/src/HelloWorld.jsx
@@ -0,0 +1,5 @@
+function HelloWorld(){
+    return <h1 className="text-center mt-5">Hello World</h1>;
+}
+
+export default HelloWorld
---
+++ ems-frontend/src/components/HeaderComponent.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const HeaderComponent = () => {
+  return (
+    <div>
+        <header>
+            <nav className='navbar navbar-dark bg-dark'>
+            <a className="navbar-brand" href="https://www.ems.net.in">
+            <img src="/flame.png" alt="Logo" height="30" className="mr-2"/>
+            Employee Management System
+              Employee Management System</a>
+            </nav>
+        </header>
+    </div>
+  )
+}
